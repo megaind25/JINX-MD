dbb8dc6094c3ea2668793f7a0254f27164c84af2
caseyweb <caseywebsthedonn25@gmail.com>
2025-04-06 12:16:39 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function downloadSessionData() {
         return false;
     }
 
-    const sessdata = config.SESSION_ID.split("KHAN-MD~")[1];
+    const sessdata = config.SESSION_ID.split("Caseyrhodes~")[1];
 
     if (!sessdata || !sessdata.includes("#")) {
         console.error('❌ Invalid SESSION_ID format! It must contain both file ID and decryption key.');
